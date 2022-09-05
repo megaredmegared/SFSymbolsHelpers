fd3b8b266dc0ad845c38a385f8cc357f9a0a55c9
Sebastien SARRAZIN <sarseb@icloud.com>
2022-09-05 12:38:11 +0200

    starting files

---
+++ Symbols.playground/Pages/PrintSymbolExtension.xcplaygroundpage/Contents.swift
@@ -0,0 +1,40 @@
+import Foundation
+
+let symbols = symbolsStrings.components(separatedBy: "\n")
+
+func printStringSymbolExtension(name: String, prefixName: String) {
+    print("extension String {\n    struct \(name) {")
+    for symbol in symbols {
+        let constant = symbol
+            .capitalized
+            .replacingOccurrences(of: ".", with: "")
+
+        print("        static let \(prefixName)\(constant) = \"\(symbol)\"")
+    }
+    print("    }\n}")
+}
+
+// Create an extension on String for the symbol name.
+
+// You can change the name of the struct and the prefix for the symbols. The prefix is needed because some symbol starts with a number
+
+printStringSymbolExtension(name: "Symbol", prefixName: "named")
+
+
+
+func printImageSymbolExtension(prefixName: String) {
+    print("extension Image {")
+    for symbol in symbols {
+        let constant = symbol
+            .capitalized
+            .replacingOccurrences(of: ".", with: "")
+
+        print("    static let \(prefixName)\(constant) = Image(systemName: \"\(symbol)\")")
+    }
+    print("}")
+}
+
+// Create the code for an extension on Image for swiftUI.
+
+// You can change the prefix for the symbols. The prefix is needed because some symbol starts with a number
+printImageSymbolExtension(prefixName: "named")
